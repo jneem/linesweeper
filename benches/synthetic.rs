3b972dbcc394d20287f3063f34aadbf116c8abf8
Joe Neeman <joeneeman@gmail.com>
2025-01-11 08:57:15 +0700

    Add i_overlay to the comparison

--- benches/synthetic.rs
+++ benches/synthetic.rs
@@ -6,8 +6,9 @@ use linesweeper::{
 };
 
 type Float = NotNan<f64>;
+type Contours = Vec<Vec<Point<Float>>>;
 
-fn cubes((x0, y0): (f64, f64), size: f64, offset: f64, count: usize) -> Vec<Vec<Point<Float>>> {
+fn cubes((x0, y0): (f64, f64), size: f64, offset: f64, count: usize) -> Contours {
     let mut ret = Vec::new();
     let x0 = NotNan::try_from(x0).unwrap();
     let y0 = NotNan::try_from(y0).unwrap();
@@ -28,61 +29,86 @@ fn cubes((x0, y0): (f64, f64), size: f64, offset: f64, count: usize) -> Vec<Vec<
     ret
 }
 
-fn checkerboard(n: usize) -> Vec<Vec<Point<Float>>> {
-    let mut ret = cubes((0.0, 0.0), 30.0, 40.0, n);
-    ret.extend(cubes((20.0, 20.0), 30.0, 40.0, n - 1));
-
-    ret
+fn checkerboard(n: usize) -> (Contours, Contours) {
+    (
+        cubes((0.0, 0.0), 30.0, 40.0, n),
+        cubes((20.0, 20.0), 30.0, 40.0, n - 1),
+    )
 }
 
 fn just_the_sweep(c: &mut Criterion) {
-    let contours = checkerboard(10);
+    let (contours_even, contours_odd) = checkerboard(10);
 
     let eps = NotNan::try_from(0.01f64).unwrap();
     let mut segs = Segments::default();
-    for c in contours {
+    for c in contours_even.into_iter().chain(contours_odd) {
         segs.add_cycle(c);
     }
 
     c.bench_function("just the sweep", |b| {
-        b.iter(|| {
-            for _ in 0..1000 {
-                linesweeper::sweep::sweep(&segs, &eps, |_, _| {})
-            }
-        })
+        b.iter(|| linesweeper::sweep::sweep(&segs, &eps, |_, _| {}))
     });
 }
 
 fn build_topology(c: &mut Criterion) {
-    let contours = checkerboard(10);
+    let (contours_even, contours_odd) = checkerboard(10);
 
     let eps = NotNan::try_from(0.01f64).unwrap();
-
-    const EMPTY: [[Point<NotNan<f64>>; 0]; 0] = [];
-
     c.bench_function("build topology", |b| {
-        b.iter(|| black_box(Topology::new(contours.clone(), EMPTY, &eps)));
+        b.iter(|| {
+            black_box(Topology::new(
+                contours_even.clone(),
+                contours_odd.clone(),
+                &eps,
+            ))
+        });
     });
 }
 
 fn xor(c: &mut Criterion) {
-    let contours: Vec<_> = checkerboard(10)
-        .into_iter()
-        .map(|ps| {
-            ps.into_iter()
-                .map(|p| (p.x.into_inner(), p.y.into_inner()))
-                .collect()
-        })
-        .collect();
+    let to_floats = |contours: Contours| -> Vec<_> {
+        contours
+            .into_iter()
+            .map(|ps| {
+                ps.into_iter()
+                    .map(|p| (p.x.into_inner(), p.y.into_inner()))
+                    .collect()
+            })
+            .collect()
+    };
+
+    let (contours_even, contours_odd) = checkerboard(10);
+    let contours_even = to_floats(contours_even);
+    let contours_odd = to_floats(contours_odd);
 
     c.bench_function("xor", |b| {
         b.iter(|| {
             black_box(boolean_op(
-                &contours,
-                &Vec::new(),
+                &contours_even,
+                &contours_odd,
                 FillRule::EvenOdd,
                 BooleanOp::Xor,
             ))
+        });
+    });
+
+    let to_float_arrays = |contours: Vec<Vec<(f64, f64)>>| -> Vec<Vec<_>> {
+        contours
+            .into_iter()
+            .map(|ps| ps.into_iter().map(|(x, y)| [x, y]).collect())
+            .collect()
+    };
+    let contours_even = to_float_arrays(contours_even);
+    let contours_odd = to_float_arrays(contours_odd);
+
+    c.bench_function("xor i_overlay", |b| {
+        b.iter(|| {
+            use i_overlay::float::single::SingleFloatOverlay;
+            contours_even.overlay(
+                &contours_odd,
+                i_overlay::core::overlay_rule::OverlayRule::Xor,
+                i_overlay::core::fill_rule::FillRule::EvenOdd,
+            );
         });
     });
 }
