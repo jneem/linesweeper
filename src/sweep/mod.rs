7c1f233e443ce198a5c3a2ec6bbfc0bdb1864508
Joe Neeman <joeneeman@gmail.com>
2025-02-08 12:14:28 +0700

    Fix comment

--- src/sweep/mod.rs
+++ src/sweep/mod.rs
@@ -1,9 +1,9 @@
 //! The sweep-line implementation.
 //!
 //! The details of this implementation are described in the `docs` directory.
-//The main entry ! point is [`Sweeper`], which computes all the intersection
-//points between a collection of ! line segments, and makes them available
-//sweep-line by sweep-line.
+//! The main entry point is [`Sweeper`], which computes all the intersection
+//! points between a collection of line segments, and makes them available
+//! sweep-line by sweep-line.
 
 mod weak_ordering;
 
