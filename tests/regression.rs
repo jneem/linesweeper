21be6c16f9c939b471c7580b0faeb6a9e3a09cb6
Mike Wilkerson <11575183+mlwilkerson@users.noreply.github.com>
2025-11-24 11:46:28 -0800

    Add serde structs for regression test case yaml declarations

--- tests/regression.rs
+++ tests/regression.rs
@@ -2,6 +2,54 @@ use kurbo::BezPath;
 use libtest_mimic::{Arguments, Failed, Trial};
 use linesweeper::binary_op;
 use std::path::{Path, PathBuf};
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+enum FillRule {
+   EvenOdd,
+   NonZero,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+enum BinaryOp {
+    Union,
+    Intersection,
+    Difference,
+    Xor,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+enum Assertion {
+   NoPanic,
+   Snapshot
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct RegressionCaseDeclaration {
+    svg_path_1: String,
+    svg_path_2: String,
+    fill_rule: FillRule,
+    op: BinaryOp,
+    assert: Option<Assertion>
+}
+
+impl RegressionCaseDeclaration  {
+    fn linesweeper_fill_rule(&self) -> linesweeper::FillRule {
+        match self.fill_rule {
+            FillRule::EvenOdd => linesweeper::FillRule::EvenOdd,
+            FillRule::NonZero => linesweeper::FillRule::NonZero,
+        }
+    }
+
+    fn linesweeper_binary_op(&self) -> linesweeper::BinaryOp {
+        match self.op {
+            BinaryOp::Union => linesweeper::BinaryOp::Union,
+            BinaryOp::Intersection => linesweeper::BinaryOp::Intersection,
+            BinaryOp::Difference => linesweeper::BinaryOp::Difference,
+            BinaryOp::Xor => linesweeper::BinaryOp::Xor,
+        }
+    }
+}
 
 fn main() {
     let args = Arguments::from_args();
@@ -12,8 +60,9 @@ fn main() {
 
 fn regression_tests() -> Vec<Trial> {
     let ws = std::env::var("CARGO_MANIFEST_DIR").unwrap();
-    let paths = glob::glob(&format!("{ws}/tests/regression/**/*.txt")).unwrap();
-    paths
+    let file_paths = glob::glob(&format!("{ws}/tests/regression/**/*.yml")).unwrap();
+
+    file_paths
         .into_iter()
         .map(|p| {
             let p = p.unwrap();
@@ -31,15 +80,14 @@ fn input_path_base(input_path: &Path) -> &Path {
 
 fn generate_regression_test(path: PathBuf) -> Result<(), Failed> {
     let input = std::fs::read_to_string(&path).unwrap();
-    let lines: Vec<_> = input.lines().collect();
-    assert_eq!(lines.len(), 2);
-    let p0 = BezPath::from_svg(lines[0]).unwrap();
-    let p1 = BezPath::from_svg(lines[1]).unwrap();
+    let case: RegressionCaseDeclaration = serde_yaml::from_str(&input).unwrap();
+    let p0 = BezPath::from_svg(case.svg_path_1.as_str()).unwrap();
+    let p1 = BezPath::from_svg(case.svg_path_2.as_str()).unwrap();
     binary_op(
         &p0,
         &p1,
-        linesweeper::FillRule::EvenOdd,
-        linesweeper::BinaryOp::Xor,
+        case.linesweeper_fill_rule(),
+        case.linesweeper_binary_op()
     )
     .unwrap();
     Ok(())
